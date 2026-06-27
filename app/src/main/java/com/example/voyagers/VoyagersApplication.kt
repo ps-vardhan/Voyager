4ae4db6b466266bae7b3ffc9be073dd00a842d97
ps_vardhan <peesarisaivardhan@gmail.com>
2026-06-27 20:18:25 +0530

    initialize application class with @HiltAndroidApp annotation for dependency injection

---
+++ app/src/main/java/com/example/voyagers/VoyagersApplication.kt
@@ -0,0 +1,10 @@
+package com.example.voyagers
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Base Application class for Hilt Dependency Injection.
+ */
+@HiltAndroidApp
+class VoyagersApplication : Application()
